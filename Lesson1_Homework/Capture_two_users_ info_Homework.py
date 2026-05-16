9357980a907b5ee5ac1002a2258dd34f91b641f5
ysobekwa <yusobekwa@gmail.com>
2026-05-16 17:25:17 +0200

    I have captured two users infor using list

--- Lesson1_Homework/Capture_two_users_ info_Homework.py
+++ Lesson1_Homework/Capture_two_users_ info_Homework.py
@@ -1,24 +1,50 @@
-# Capture information for User 1
-name1 = input("Enter first user's name: ")
-age1 = int(input("Enter first user's age: "))
-height1 = float(input("Enter first user's height in cm: "))
+# Capture information for two users
+
+users = [
+    {
+        'name': 'Asa',
+        'age': 15,
+        'height': 156.2
+    },
+    {
+        'name': 'Tania',
+        'age': 20,
+        'height': 178.0
+    }
+]
+print(users)
+print (type(users))
+print()
+
+#display users info using a loop
+
+#for i, user in enumerate(users,1):
+       # print(f"{i} user['name'], user['age' ]}, user['height in cm']" )
+
+
+#display
+
+#name1 =[ input("Enter first user's name: ") ,age1 = int(input("Enter first user's age: ")),height1 = float(input("Enter first user's height in cm: "))]
+#age1 = int(input("Enter first user's age: "))
+#height1 = float(input("Enter first user's height in cm: "))
 
 print()
 
 # Capture information for User 2
-name2 = input("Enter second user's name: ")
-age2 = int(input("Enter second user's age: "))
-height2 = float(input("Enter second user's height in cm: "))
+#name2 = input("Enter second user's name: ")
+#age2 = int(input("Enter second user's age: "))
+#height2 = float(input("Enter second user's height in cm: "))
 
-print()
+#print()
+
 
 # Display user information
-print(f"{name1} is {age1} years old and is {height1} cm tall.")
-print(f"{name2} is {age2} years old and is {height2} cm tall.")
+#print(f"{name1} is {age1} years old and is {height1} cm tall.")
+#print(f"{name2} is {age2} years old and is {height2} cm tall.")
 
-print()
+#print()
 
 # Calculate and display total combined height
-total_height = height1 + height2
+#total_height = height1 + height2
 
-print(f"The total combined height of both users is {total_height} cm.")
+#print(f"The total combined height of both users is {total_height} cm.")
