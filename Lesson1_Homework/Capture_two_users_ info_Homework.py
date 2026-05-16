72dbf165be36e92921c4057c4420b3b9cac5e5dd
ysobekwa <yusobekwa@gmail.com>
2026-05-16 20:36:24 +0200

    calculate & display total combined height for both users

--- Lesson1_Homework/Capture_two_users_ info_Homework.py
+++ Lesson1_Homework/Capture_two_users_ info_Homework.py
@@ -20,57 +20,19 @@ users.append ({
     'age': age,
     'height': height
 })
-
 print(type(users))
 print()
-
+# display each user information in as sentence using a loop
 for user in users:
     print(f"{user['name']} is {user['age']} years old and {user['height']} cm tall.")
+    print()
+
+#calculate and display the total combined height for users
+def total_height(users) :
+    total = 0
+    for user in users:
+        total += user['height']
+    return total
+print(f"The total combined height of both users is {total_height(users)} cm ")
 
 
-
-
-    #    'age': 15,
-        #'height': 156.2
-    #{
-     #   'name': 'Tania',
-     #   'age': 20,
-     #   'height': 178.0
-    #}
-#]
-#print(users)
-#print (type(users))
-#rint()
-
-#display users info using a loop
-
-#for i, user in enumerate(users,1):
-       # print(f"{i} user['name'], user['age' ]}, user['height in cm']" )
-
-
-#display
-
-#name1 =[ input("Enter first user's name: ") ,age1 = int(input("Enter first user's age: ")),height1 = float(input("Enter first user's height in cm: "))]
-#age1 = int(input("Enter first user's age: "))
-#height1 = float(input("Enter first user's height in cm: "))
-
-print()
-
-# Capture information for User 2
-#name2 = input("Enter second user's name: ")
-#age2 = int(input("Enter second user's age: "))
-#height2 = float(input("Enter second user's height in cm: "))
-
-#print()
-
-
-# Display user information
-#print(f"{name1} is {age1} years old and is {height1} cm tall.")
-#print(f"{name2} is {age2} years old and is {height2} cm tall.")
-
-#print()
-
-# Calculate and display total combined height
-#total_height = height1 + height2
-
-#print(f"The total combined height of both users is {total_height} cm.")
